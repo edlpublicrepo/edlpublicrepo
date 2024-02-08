91380c6d83cc4e00f2e7a2dc6e9161f2d3808f14
Erick Lima <limaerir@amazon.com>
2024-02-08 09:48:07 -0800

    adding hit count feature to website

--- edlresume.com/iac/toSyncToS3/js/scripts.js
+++ edlresume.com/iac/toSyncToS3/js/scripts.js
@@ -1,12 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Call API to get cost of website
     fetch('https://6dfeb7uf1f.execute-api.us-east-2.amazonaws.com/prod')
     .then(response => response.text()) // assuming the API returns HTML formatted text
     .then(data => {
-        document.getElementById('apiResponse').innerHTML = data;
+        document.getElementById('apiResponseWebsiteCost').innerHTML = data;
     })
     .catch(error => {
         console.error('There was an error fetching the API data:', error);
-        document.getElementById('apiResponse').innerText = 'Failed to load costs.';
+        document.getElementById('apiResponseWebsiteCost').innerText = 'Failed to load costs.';
+    });
+    // Call API to get traffic on website
+    fetch('https://jdwksop5g9.execute-api.us-east-2.amazonaws.com/prod')
+    .then(response => response.text()) // assuming the API returns HTML formatted text
+    .then(data => {
+        document.getElementById('apiResponseWebsiteTraffic').innerHTML = data;
+    })
+    .catch(error => {
+        console.error('There was an error fetching the API data:', error);
+        document.getElementById('apiResponseWebsiteTraffic').innerText = 'Failed to load costs.';
     });
 });
 
