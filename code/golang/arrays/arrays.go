b41b801af89df88e94c1d8831cdb7ae2235d25f3
Erick Lima <limaerir@amazon.com>
2024-02-29 11:35:35 -0800

    uploading arrays folder

---
+++ code/golang/arrays/arrays.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a [5]int
+	fmt.Println(a)
+
+	a[4] = 100
+	fmt.Println("Set:", a)
+	fmt.Println("Get:", a[4])
+
+	fmt.Println("Length:", len(a))
+
+	num1 := 1
+	b := [5]int{0, num1, 2, 3, 4}
+	fmt.Println("b:", b)
+
+	var twoDimensional [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoDimensional[i][j] = i + j
+			fmt.Println("testing", twoDimensional)
+		}
+	}
+	fmt.Println(twoDimensional)
+}
